5c54a124a732283276a2a552187b2a053cb4d501
hagukin <hagukin@naver.com>
2021-08-18 02:29:05 +0900

    increase max depth to 20 for playtesting

--- src/biome_by_depth.py
+++ src/biome_by_depth.py
@@ -1,7 +1,7 @@
 from typing import Dict, Optional
 
 def get_dungeon_biome(depth: int) -> Optional[Dict]:
-    if depth == 10:
+    if depth == 20:
         return {
             "ancient_ruins":1
         }
